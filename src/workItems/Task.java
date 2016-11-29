2f60fa15785f03fefdddb3b9a015a3e250546c2f
gokhanozden <gokhan.ozden@yahoo.com>
2016-11-29 09:51:32 -0500

    Comments added

--- src/workItems/Task.java
+++ src/workItems/Task.java
@@ -5,37 +5,43 @@ import serviceProviders.ServiceProviderAgent;
 import xtext.objectsModel.Skill;
 import xtext.objectsModel.WorkItem;
 
-// To Gohkan: 
+// To Gokhan: 
 // This file inherits from WorkItemEntity.java. 
 // This file is also the parent class for: DevTask/AnalysisActivity/ResolutionActivity
 
 public class Task extends WorkItemEntity{
-	//This is used for connecting DSL with simulation model
+	//Gokhan: isAggregationNode is used for connecting DSL with simulation model
 	protected boolean isAggregationNode = false;
+	//Gokhan: What does current decision do
 	public int currentDecision; //1=Accept,2=Outsource,3=RequestHelp,4=Decline
-	
+	//Gokhan: constructor for Task class
 	public Task(WorkItem wi) {
 		super(wi);
 	}
+	//Gokhan: add resources to task entity
 	public void allocateResource(ResourceEntity sR) {
 		this.getAllocatedResources().add(sR);
 		//System.out.println("Resource "+sR.getName()+" is Allocated to "+this.getName());
 	}
+	//Gokhan: delete resources from task entity
 	public void withdrawResource(ResourceEntity sR) {
 		this.getAllocatedResources().remove(sR);
 		//System.out.println("Resource "+sR.getName()+" is Withdrawed from "+this.getName());
 	}
+	//Gokhan: explain what does this method do, what is sEfficiency, also explain why you used hash map (for efficiency purpose)
 	public double calculateExtendedServiceCapacity(ServiceProviderAgent sp) {
 		double sEfficiency = 0;
 		sEfficiency = sp.ExtendedServiceCapacity.get(SoS.myServices.get(this.serviceId))
 				+ sp.ServiceCapacity.get(SoS.myServices.get(this.serviceId));
 		return sEfficiency;
 	}
+	//Gokhan: explain what does this method do
 	public double calculateServiceCapacity(ServiceProviderAgent sp) {
 		double sEfficiency = 0;
 		sEfficiency = sp.ServiceCapacity.get(SoS.myServices.get(this.serviceId));
 		return sEfficiency;
 	}
+	//Gokhan: explain what does the resource efficiency do, how it is calculated, add comments inside the method if necessary
 	public double calculateResourceEfficiency() {
 		double myEfficiency = 0;
 		int totalResources = this.getAllocatedResources().size();
@@ -50,6 +56,7 @@ public class Task extends WorkItemEntity{
 		}
 		return myEfficiency;
 	}
+	//Gokhan: how do you advance progress, add comment to each line below
 	public void advanceProgress() {
 		// ------------ Compute WI Progress (percentage) -----------	
 		if (this.isStarted) {
@@ -60,6 +67,7 @@ public class Task extends WorkItemEntity{
 				((ResolutionActivity)this).ResolutionObject.cycleTime += 1;
 			}
 			//System.out.println(this.getName()+"(CycleTime:"+this.cycleTime+") at "+this.getAllocatedResources().get(0).getName()+" progress rate "+this.progressRate);
+			//Gokhan: Why there is a magic number in this calculation?
 			setProgress(getProgress() + getProgressRate() + 0.000001);					
 			if (getProgress() >= 1) {			
 				setProgress(1.00);	
@@ -68,13 +76,15 @@ public class Task extends WorkItemEntity{
 			//{System.out.println(this.fullName+"Maturity Level:"+doubleMaturityLevels+" which is "+(int)(doubleMaturityLevels));}
 			setCurrentMaturityLevel((int)(doubleMaturityLevels));	
 		}
-	}	
+	}
+	//Gokhan: why there is this method, what is the need for it?
 	public void withdrawAllResources() {
 		while (!this.getAllocatedResources().isEmpty()) {
 			ResourceEntity resource = this.getAllocatedResources().getFirst();
 			resource.withdrawFrom(this);
 		}
 	}
+	//Gokhan: when do you call this method, what is the purpose of this method
 	public void setStarted() {	
 		if (this.isStarted) {
 			this.isRestarted = true;
